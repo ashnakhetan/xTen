f6741354e4e6f122ec4d581b8f3631b0f811e072
noelsarduy <noelsd@stanford.edu>
2023-04-29 14:57:49 -0700

    Added setup.sh script and updated @xten readme

--- @xten/README.md
+++ @xten/README.md
@@ -1,5 +1,4 @@
-Initial set up for locally testing the npm package
-1- Run npm install \ 
-2- Run npm link @xten\xten under @xten folder (mat require sudo) \
-3- Go to meet-normal-bedbug and run npm link \
-4- Package should be linked and ready to use in the extension \
+To initialize testing make sure setup.sh has execute file permissions: \
+chmod +x setup.sh \
+Then run the script: \
+./setup.sh
---
+++ @xten/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run npm install
+echo "Running npm install in @xten folder..."
+npm install
+
+# Run npm link
+echo "Running npm link in @xten folder..."
+sudo npm link
+
+# Navigate to the meet-normal-bedbug folder
+cd ../meet-normal-bedbug
+
+# Run npm link to connect the package
+echo "Running npm link to connect @xten package in meet-normal-bedbug folder..."
+npm link
+
+# Confirm that the package is linked and ready to use
+echo "Package linked and ready to use in the extension."
