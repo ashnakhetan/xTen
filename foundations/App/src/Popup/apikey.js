8d87bd5c7011649aee1afae4c1694d342e4f13e0
ruslanjabari <rjabari@stanford.edu>
2023-05-09 16:00:54 -0700

    rip

--- foundations/App/src/Popup/apikey.js
+++
@@ -1,3 +0,0 @@
-const API_KEY='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';
-
-export default API_KEY;
--- foundations/Firefox/public/build/content.js
+++
@@ -1,2 +0,0 @@
-(()=>{function r(o){try{let e=document.head||document.documentElement,t=document.createElement("script");t.setAttribute("async","false"),t.setAttribute("type","text/javascript"),t.setAttribute("src",o),e.insertBefore(t,e.children[0]),e.removeChild(t)}catch(e){console.error(`Failed to inject script
-`,e)}}console.log("content script");r(chrome.runtime.getURL("/build/injected.js"));})();
