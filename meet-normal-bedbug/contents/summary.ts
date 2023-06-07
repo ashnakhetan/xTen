7c6b4864b838e0074ef31605fb95b82b996a3310
noelsarduy <noelsd@stanford.edu>
2023-06-07 16:48:59 -0700

    Added background script, custom plugin store

--- meet-normal-bedbug/contents/summary.ts
+++ meet-normal-bedbug/contents/summary.ts
@@ -1,20 +1,32 @@
-export {}
-
-import {SummarizerPlugin } from '../../@xten/src/plugins/summarizer/summarizerPlugin';
-
+import { SummarizerPlugin } from '../../@xten/src/plugins/summarizer/summarizerPlugin';
 
 const apiKey = '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';
 const summarizerPlugin = new SummarizerPlugin(apiKey, true);
 
-// Attach the imported plugin to the page
-summarizerPlugin.attach();
+let enabled = true;  // Defaults to enabled
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.command == "attach") {
-        // Code for attaching event listener or functionality goes here
+console.log("Background script is running"); // Add this line to your script
+
+// Initially fetch the state from the storage
+chrome.storage.local.get(['smartSummarizer'], function(result) {
+    console.log('Value currently is ' + result.smartSummarizer);
+  enabled = !!result.smartSummarizer;
+  console.log("Enabled: " + enabled);
+    if (enabled) {
         summarizerPlugin.attach();
-    } else if (request.command == "detach") {
-        // Code for detaching event listener or functionality goes here
-        summarizerPlugin.detach();
-    }
+        }
+});
+
+// Listen for messages from the popup
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.command == "attach") {
+    enabled = true;
+    summarizerPlugin.attach();
+  } else if (request.command == "detach") {
+    enabled = false;
+    summarizerPlugin.detach();
+  }
+  
+  // Update the state in the storage
+  chrome.storage.local.set({ 'smartSummarizer': enabled });
 });
