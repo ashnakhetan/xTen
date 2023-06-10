84b3db351a69e15458a2c08b89b9b21df78e147d
Ishita <ishita@Ishitas-MacBook-Pro-3.local>
2023-06-09 19:31:51 -0700

    moving mkdocs.yml

--- meet-normal-bedbug/contents/content.ts
+++ meet-normal-bedbug/contents/content.ts
@@ -1,36 +1,25 @@
 import type { PlasmoCSConfig, PlasmoCSUIProps, PlasmoRender } from "plasmo"
 import type { FC } from "react"
 import { createRoot } from "react-dom/client"
+ 
+import xten from "/Users/ishita/2023-87Capital/@xten"
 
-import xten from "~../@xten"
-
-const PlasmoOverlay: FC<PlasmoCSUIProps> = () => {
-  return (
-    <span
-      style={{
-        background: "red",
-        padding: 12
-      }}>
-      HELLO WORLD ROOT CONTAINER
-    </span>
-  )
-}
-
-const i = xten.Injector
+var i = xten.Injector
 
 // configure url matches
 i.configure({
   matches: ["https://www.utu.fi/*"]
 })
+
 // get root container by id
 i.injectById("block-herotitleblock")
 // inject element
 i.renderElem(<PlasmoOverlay />)
-//
 
 export const config = (() => i.getConfig())()
 export const getRootContainer = (() => i.getRootContainerConfig())()
 export const render = (() => i.getRenderer())()
+
 
 // Replace the URL below with the website you want to scrape
 // const url = 'https://najm-eddine-zaga.medium.com/18-best-practices-for-react-617e23ed7f2c';
